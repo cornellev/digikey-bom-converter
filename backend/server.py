690e7a8f0f98f5e49c66807556e4213c72c8f7d4
Katie Xiao <jx385@cornell.edu>
2026-03-07 14:19:58 -0500

    cors stuff

--- backend/server.py
+++ backend/server.py
@@ -17,6 +17,7 @@ app.add_middleware(
         'http://127.0.0.1:5173',
         'https://localhost:5173',
         'https://127.0.0.1:5173',
+        'https://cornellev.github.io'
     ],
     allow_credentials=True,
     allow_methods=['*'],
