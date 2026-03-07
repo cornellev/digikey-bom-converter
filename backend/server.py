60dad8127478c00400b51bb8ee3510f34a782f28
Katie Xiao <jx385@cornell.edu>
2026-03-07 14:40:41 -0500

    fix comment issue

--- backend/server.py
+++ backend/server.py
@@ -12,21 +12,21 @@ from bom_service import convert_uploaded_bom
 app = FastAPI(title='DigiKey BOM Converter Backend', version='0.1.0')
 app.add_middleware(
     CORSMiddleware,
-    """
-    allow_origins=[
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'https://localhost:5173',
-        'https://127.0.0.1:5173',
-        'https://cornellev.github.io/digikey-bom-converter'
-    ],
-    """
     allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
 
+"""
+allow_origins=[
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://localhost:5173',
+    'https://127.0.0.1:5173',
+    'https://cornellev.github.io/digikey-bom-converter'
+],
+"""
 
 @app.get('/health')
 def health() -> dict:
