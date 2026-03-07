4154521dad8efe0d49263620735eafb4d89fa523
Katie Xiao <jx385@cornell.edu>
2026-03-07 14:28:41 -0500

    more cors stuff

--- backend/server.py
+++ backend/server.py
@@ -17,7 +17,7 @@ app.add_middleware(
         'http://127.0.0.1:5173',
         'https://localhost:5173',
         'https://127.0.0.1:5173',
-        'https://cornellev.github.io'
+        'https://cornellev.github.io/digikey-bom-converter'
     ],
     allow_credentials=True,
     allow_methods=['*'],
